b6383c5123ff80a8857d01836300cddb27552ae5
Ujjwal <ujwaldua999@gmail.com>
2023-02-23 16:07:47 +0530

    codechange2.0

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./Components/Navbar"
+import Home from "./Components/Home"
+import Text from "./Components/Text"
+import About from "./Components/About"
+import Class from "./Components/Class"
+import Schdule from './Components/Schdule';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+import Modal from './Components/Modal';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar></Navbar>
+   <Home> </Home>
+   <Text></Text>
+   <About></About>
+   <Class></Class>
+   <Schdule></Schdule>
+   <Contact></Contact>
+   <Footer></Footer>
+   <Modal></Modal>
+    </>
+   
   );
 }
 
